d0e04b0e50c4801e787cbfd0ed2b876567715512
chekos <sergio@cimarron.io>
2022-09-17 13:58:06 -0700

    adjust test to pass in ubuntu too

--- tests/test_esporifai.py
+++ tests/test_esporifai.py
@@ -12,5 +12,5 @@ runner = CliRunner()
 def test_help(options):
     result = CliRunner().invoke(cli.cli, options)
     assert result.exit_code == 0
-    assert "Usage: " in result.output.strip()
+    assert "Usage: " in result.output
     assert "--help" in result.output
