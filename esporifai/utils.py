720d874e474ba7a386fd85172c39a5ad4d51a9f7
chekos <sergio@cimarron.io>
2022-10-05 16:55:51 -0700

    expand timeout

--- esporifai/utils.py
+++ esporifai/utils.py
@@ -42,7 +42,7 @@ def retrieve_code(write: bool = False):
             page.locator("[data-testid=auth-accept]").click(timeout=3_000)
         except:
             pass
-        page.wait_for_url(f"{REDIRECT_URI}**")
+        page.wait_for_url(f"{REDIRECT_URI}**", timeout=60_000)
 
         # Authorized page
         auth = {}
